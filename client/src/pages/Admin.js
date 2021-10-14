784f97602e283aafe6a25819fa0449735fa5b968
Nicholas Dullam <ndullam@purdue.edu>
2021-10-14 17:25:03 -0400

    Forgot to remove the reference :(

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Admin = (props) => {
+    return (
+        <div>
+            <div style={{ position: 'sticky', padding: '50px', justifyContent: 'center' }}>
+                <div style={{ padding: '10px', borderRadius: '15px', backgroundColor: 'black' }}>
+                    <h6> Testing </h6>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Admin
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,11 +3,13 @@ import Login from './Login'
 import Home from './Home'
 import ProductListing from './ProductListing'
 import Signup from './Signup'
+import Admin from './Admin'
 
 export { 
     Checkout,
     Login,
     Home,
     ProductListing,
-    Signup
+    Signup,
+    Admin
 }
