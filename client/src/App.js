3783f4d04bee861f75e33e4f6dec7c61edd3f0e9
Victoire <victoire.beaufil@gmail.com>
2021-10-14 10:05:08 -0400

    Added token and user._id to frontend, Navbar Links vary based on user login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link,  } from "react-router-dom";
 import React, { useState, useCallback, useEffect } from 'react';
 
 
@@ -10,37 +10,49 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ProductListing from "./pages/ProductListing";
 import AuthContext from "./context/auth-context";
+import Messages from "./pages/Messages";
+import Profile from './pages/Profile';
 
 let logoutTimer;
 
+var logged = false;
+
 
 function App() {
+
   const [token, setToken] = useState(false);
   const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token, expirationDate) => {
+  const login = useCallback((token, user) => {
+
     console.log("LOGGING IN Y'ALLL")
-    setToken(token);
-    setUserId(uid);
-    const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpirationDate(tokenExpirationDate);
+    console.log("token: " + token + ", user: " + user)
+     setToken(token);
+    
+    // const tokenExpirationDate =
+    //   expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    // setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem(
       'userData',
       JSON.stringify({
-        userId: uid,
+        userId: user._id,
         token: token,
-        expiration: tokenExpirationDate.toISOString()
+        // expiration: tokenExpirationDate.toISOString()
       })
     );
+    setUserId(user._id);
+logged = true;
+
   }, []);
+
 
   const logout = useCallback(() => {
     setToken(null);
     setTokenExpirationDate(null);
     setUserId(null);
     localStorage.removeItem('userData');
+    logged = false;
   }, []);
 
   useEffect(() => {
@@ -72,10 +84,16 @@ function App() {
       login: login,
       logout: logout
     }}>
-
       {/* If (token) to restrict access to routes from unlogged users */}
     <Router>
       <Switch>
+      <Route path="/messages" >
+        <Messages/>
+        </Route>
+        <Route path="/profile" >
+        <Profile/>
+        </Route>
+
         <Route path="/login" >
         <Login></Login>
           </Route>
---
+++ client/src/pages/Messages.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Messages = (props) => {
+    return (
+
+        <h1>Messages</h1>
+    )
+}
+
+export default Messages
