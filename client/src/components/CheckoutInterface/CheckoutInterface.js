74a5f7348d067567c5ba401fd528435c2d9968d6
Nicholas Dullam <ndullam@purdue.edu>
2021-10-14 09:22:06 -0400

    General stripe setup on client, index.js files for components and pages

---
+++ client/src/components/CheckoutInterface/CheckoutInterface.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CheckoutInterface = (props) => {
+    return (
+        <div/>
+    )
+}
+
+export default CheckoutInterface
---
+++ client/src/components/index.js
@@ -0,0 +1,15 @@
+import CheckoutInterface from './CheckoutInterface/CheckoutInterface'
+import GameCard from './GameCard/GameCard'
+import GameGallery from './GameGallery/GameGallery'
+import Layout from './Layout/Layout'
+import Listing from './Listing/Listing'
+import Navbar from './Navbar/Navbar'
+
+export default {
+    CheckoutInterface,
+    GameCard,
+    GameGallery,
+    Layout,
+    Listing,
+    Navbar
+}
