2108dabe9364a96fbe6fab8251216fbac093d0a9
Victoire <victoire.beaufil@gmail.com>
2021-11-29 16:16:57 -0500

    Added frontend for cookie pop-up & cookie checkbox in preferences page

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../Layout/Layout.css";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import CookieCard from "../CookieCard/CookieCard";
 
 const Layout = (props) => {
     return (
@@ -14,6 +15,7 @@ const Layout = (props) => {
                 <div className="container" style={{ paddingTop: '40px', marginBottom: '40px', height: '100%', display: 'flex', flexDirection: 'column', flexGrow: '1' }}>
                     {props.children}
                 </div>
+                <CookieCard/>
                 <Footer/>
             </div>
         </div>
